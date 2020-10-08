a0209c270eacb4aa2db765679d913dd5aa92af63
shripada <shripada@robosoftin.com>
2020-10-08 11:56:09 +0530

    React app and prop type exerises

---
+++ hello-react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Box from './Box';
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+    return (
+        <div className="App">
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+        </div>
+    );
+}
+
+export default App;
---
+++ hello-react/src/Box.js
@@ -0,0 +1,9 @@
+import './Box.css';
+
+import React from 'react';
+
+const Box = () => {
+    return <div className={'Box'}></div>;
+};
+
+export default Box;
